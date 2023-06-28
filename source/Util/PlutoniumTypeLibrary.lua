7c7f0852b030727a9f9a32421ff0d5d360601dd3
YEAH-IM-PAUL <127816226+YEAH-IM-PAUL@users.noreply.github.com>
2023-06-28 08:50:56 -0500

    Create PlutoniumTypeLibrary.lua

---
+++ source/Util/PlutoniumTypeLibrary.lua
@@ -0,0 +1,67 @@
+export type InstancePool = {
+	Template: Instance,
+	InUse: any,
+	Available: any,
+	Container: Instance,
+	ExpansionSize: number;
+}
+
+export type Caster = {
+	CastUpdated: RBXScriptSignal,
+	RayHit: RBXScriptSignal,
+	RayPierced: RBXScriptSignal,
+	CastStopping: RBXScriptSignal,
+	Fire: (Vector3, Vector3, number, KwikData) -> CastInstance
+}
+
+export type PrimerData = {
+	RaycastParams: RaycastParams,
+	Acceleration: Vector3,
+	SimulationSpeed: number,
+	TracerPool: InstancePool?,
+	MaxDistance: number,
+	UserData: {any},
+	PierceFunction: any,
+}
+
+export type CastInstance = {
+	Caster: Caster;
+	SimulationSpeed: number;
+	
+	Position: Vector3,
+	Velocity: Vector3,
+	CastId: number,
+	MaxDistance: number,
+	Distance: number,
+	
+	Trajectories: {PrimerTrajectory},
+	
+	Tracer: nil;
+	PartPool: InstancePool,
+	RaycastParams: RaycastParams,
+	PierceFunction: (CastInstance, RaycastResult, Vector3, Vector3) -> boolean,
+	
+	UserData: {any},
+	
+	Active: boolean,
+	
+	EditTrajectory: (Vector3, Vector3, Vector3) -> nil
+}
+
+export type PrimerTrajectory = {
+	Origin: Vector3,
+	InitialVelocity: Vector3,
+	StartTick: number,
+	LastTick: number,
+	Acceleration: Vector3
+}
+
+export type LoomInstance = {
+	Name: string,
+	Object: DataStore,
+	Type: string,
+	Save: (any, {any}) -> nil,
+	Export: (any) -> {any},
+}
+
+return {}
